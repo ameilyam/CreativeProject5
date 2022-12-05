7a13c1487ce865ea9d4cdc5a78bffcd6974510ce
Ameilya Monson <ameilyamonson@gmail.com>
2022-12-05 09:50:34 -0700

    created front-end and got navbar up

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./NavBar.js"
+
+function App() {
+  return (
+    <div data-theme="cupcake" className="App">
+      <header class="header">
+        <NavBar/>
+      </header>
+      <h2>
+          Welcome  to  Designs Meez!
+      </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*,{js, jsx, ts, tsx",
+    'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "acid",
+      "cupcake",
+    ],
+  },
+}
